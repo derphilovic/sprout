594d1aab33ea40afb42be415d127ca2f28b1dbc1
derphilovic <philovic@outlook.de>
2025-08-29 15:59:29 +0200

    first sprout code and print

--- main.py
+++ main.py
@@ -2,20 +2,44 @@
 import os
 
 #startup variables
-cVar = {}
+cVar = {'i': 'HALP'}
 #functions
+#integer module
 def integ(value):
     if " = " in value:
         name, val = value.split(" = ", 1)
         cVar[str(name)] = int(val)
 
+#string module
 def string(value):
     if " = " in value:
         name, val = value.split(" = ", 1)
         cVar[str(name)] = str(val)
 
-elements = {'int' : integ,
-            'str' : string}
+#print module
+def mprint(value):
+    value = value.removeprefix(": ")
+    if value in cVar:
+        temp = cVar.get(f"{value}")
+        print(temp)
+    else :
+        print(value)         
 
-a = "hoj = test"
-string(a)
+#module mapping
+elements = {'int' : integ,
+            'str' : string,
+            'print' : mprint}
+
+# example program
+program = [
+    "int i = 4",
+    "str txt = John",
+    "print : i",
+    "print : txt",
+    "print : Hello World"
+]
+
+for line in program:
+    for key in elements:
+        if line.startswith(key):
+            elements[key](line[len(key):].strip())
